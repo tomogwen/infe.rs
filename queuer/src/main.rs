63a9013fab284a383e88315ebca081128f1cd9ec
tomogwen <tdavies@turing.ac.uk>
2024-07-23 22:17:57 +0100

    curling rust service inside docker

--- queuer/src/main.rs
+++ queuer/src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello World! - queuer");
+use actix_web::{get, post, App, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!\n")
+}
+
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    println!("Launching webserver");
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+            .service(echo)
+    })
+    .bind(("0.0.0.0", 8000))?
+    .run()
+    .await
 }
